66d34c0c903bee6d303a62249d1deb8a2199531b
jeffshee <jeffshee8969@gmail.com>
2020-05-28 18:32:01 +0900

    Install script

--- install.sh
+++ install.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
+if [ -d "$HOME/.hidamari" ]; then
+  rm -rf ~/.hidamari
+fi
 git clone https://github.com/jeffshee/hidamari.git ~/.hidamari
 mkdir ~/bin
+if [ -f "$HOME/bin/hidamari" ]; then
+  rm ~/bin/hidamari
+fi
 ln -s ~/.hidamari/hidamari ~/bin/hidamari
 cp ~/.hidamari/hidamari.desktop ~/.config/autostart/hidamari.desktop
 cp ~/.hidamari/hidamari.desktop ~/.local/share/applications/hidamari.desktop
 cp ~/.hidamari/hidamari.svg ~/.local/share/icons/hicolor/scalable/apps/hidamari.svg
-
