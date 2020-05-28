84faae4575daf7c64d4be7dee023080182d861c2
jeffshee <jeffshee8969@gmail.com>
2020-05-29 02:51:39 +0900

    Install Script

--- install.sh
+++ install.sh
@@ -1,15 +1,33 @@
 #!/bin/sh
-if [ -d "$HOME/.hidamari" ]; then
-  rm -rf ~/.hidamari
-fi
-git clone https://github.com/jeffshee/hidamari.git ~/.hidamari
-if [ ! -d "$HOME/bin" ]; then
-  mkdir ~/bin
-fi
-if [ -f "$HOME/bin/hidamari" ]; then
-  rm ~/bin/hidamari
-fi
-ln -s ~/.hidamari/hidamari ~/bin/hidamari
-cp ~/.hidamari/hidamari.desktop ~/.config/autostart/hidamari.desktop
-cp ~/.hidamari/hidamari.desktop ~/.local/share/applications/hidamari.desktop
-cp ~/.hidamari/hidamari.svg ~/.local/share/icons/hicolor/scalable/apps/hidamari.svg
+
+install() {
+  if [ -d "$HOME/.hidamari" ]; then
+    rm -rf ~/.hidamari
+  fi
+  git clone https://github.com/jeffshee/hidamari.git ~/.hidamari
+  if [ ! -d "$HOME/bin" ]; then
+    mkdir ~/bin
+  fi
+  if [ -f "$HOME/bin/hidamari" ]; then
+    rm ~/bin/hidamari
+  fi
+  ln -s ~/.hidamari/hidamari ~/bin/hidamari
+  cp ~/.hidamari/hidamari.desktop ~/.config/autostart/hidamari.desktop
+  cp ~/.hidamari/hidamari.desktop ~/.local/share/applications/hidamari.desktop
+  cp ~/.hidamari/hidamari.svg ~/.local/share/icons/hicolor/scalable/apps/hidamari.svg
+}
+
+echo "=== Hidamari Installer Script ==="
+echo "This script install hidamari to $HOME/.hidamari"
+while true; do
+  read -r -p "Do you wish to proceed [y/N]?" yn
+  case $yn in
+  [Yy]*)
+    install
+    break
+    ;;
+  *)
+    echo "Abort"
+    exit ;;
+  esac
+done
